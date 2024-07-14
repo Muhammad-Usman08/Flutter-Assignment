28d6c157ed5a0cdbb456ea043841201e4402aa5d
musman-js <usman2037966@gmail.com>
2024-07-14 20:56:22 +0500

    Ui changes remaining

--- calculator/lib/components/button.dart
+++ calculator/lib/components/button.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  String number;
-  MyButton({super.key, required this.number});
+  final String number;
+  const MyButton({super.key, required this.number});
 
   @override
   Widget build(BuildContext context) {
@@ -11,38 +11,15 @@ class MyButton extends StatelessWidget {
       child: ElevatedButton(
         onPressed: () {},
         child: Container(
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           child: Text(
-            '$number',
+            number,
             style: const TextStyle(
               fontSize: 20,
             ),
           ),
         ),
       ),
-    );
-  }
-}
-
-class YellowBtn extends StatelessWidget {
-  String number;
-  YellowBtn({super.key, required this.number});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 10, top: 10),
-      child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFFff9f0a)),
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              '$number',
-              style: const TextStyle(fontSize: 20, color: Colors.white),
-            ),
-          )),
     );
   }
 }
--- calculator/lib/screens/home.dart
+++ calculator/lib/screens/home.dart
@@ -4,89 +4,113 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+//Calculation Function
+  calculation() {}
+
+  //button Widget
+  // Widget buttonText(){
+  //   return (
+
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      //AppBar
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: const Text(
           'Simple Calculator',
-          style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+          style: TextStyle(
+              fontWeight: FontWeight.w600, color: Colors.white, fontSize: 20),
         ),
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.only(left: 35, right: 35, bottom: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.all(22.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  fillColor: Colors.white,
-                  filled: true,
-                ),
+      // body Started
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.only(bottom: 30),
+            child: const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 43,
+                color: Colors.white,
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MyButton(number: 'C'),
-                MyButton(number: 'Del'),
-                MyButton(number: '%'),
-                YellowBtn(number: '/')
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MyButton(number: '7'),
-                MyButton(number: '8'),
-                MyButton(number: '9'),
-                YellowBtn(number: 'x')
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MyButton(number: '4'),
-                MyButton(number: '5'),
-                MyButton(number: '6'),
-                YellowBtn(number: '-')
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MyButton(number: '1'),
-                MyButton(number: '2'),
-                MyButton(number: '3'),
-                YellowBtn(number: '+')
-              ],
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Container(
-                  margin: const EdgeInsets.only(left: 10, top: 10),
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 50, vertical: 10),
-                        child: const Text(
-                          '0',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ))),
-              MyButton(number: '.'),
-              YellowBtn(number: '='),
-            ])
-          ],
-        ),
+          ),
+
+          //Buttons
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyButton(
+                number: 'C',
+              ),
+              MyButton(
+                number: '/',
+              ),
+              MyButton(
+                number: 'x',
+              ),
+              MyButton(
+                number: 'Del',
+              ),
+            ],
+          ),
+           const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyButton(
+                number: '7',
+              ),
+              MyButton(
+                number: '8',
+              ),
+              MyButton(
+                number: '9',
+              ),
+              MyButton(
+                number: '-',
+              ),
+            ],
+          ),
+           const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyButton(
+                number: '4',
+              ),
+              MyButton(
+                number: '5',
+              ),
+              MyButton(
+                number: '6',
+              ),
+              MyButton(
+                number: '+',
+              ),
+            ],
+          ),
+           const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyButton(
+                number: '1',
+              ),
+              MyButton(
+                number: '2',
+              ),
+              MyButton(
+                number: '3',
+              ),
+              MyButton(
+                number: '=',
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
